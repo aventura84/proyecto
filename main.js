10d22bac312a920c8b9f6295e8f096d381ef7831
aventura84 <ventura_antonio@hotmail.com>
2022-06-24 17:59:19 +0200

    js

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
 const input = document.querySelector("input");
 const button = document.querySelector("button");
-const pokemonContainer = document.ct(".pokemon-container")
+const pokemonContainer = document.ct(".pokemon-container");
 
-function traerPokemon(){
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=1126')
-    .then (res =>)
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  filterPokemon(input.value);
+});
+function filterPokemon(pokemon) {
+  fetch("https://pokeapi.co/api/v2/pokemon/1/")
+    .then((res) => res.json())
+    .then((data) => {
+      createPokemon(data);
+    });
 }
-
-function (){
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=1126')
-
+function getPokemon() {
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=1126");
 }
-//
+function createPokemon(pokemon) {
+  const img = document.createElement("img");
+  img.src = pokemon.sprites.front_default;
+  const h3 = document.createElement("h3");
+  h3.textContent = pokemon.name;
+  const div = document.createElement("div");
+  div.appendChild(img);
+  div.appendChild(h3);
+  pokemonContainer.appendChild(div);
+}
+filterPokemon();
