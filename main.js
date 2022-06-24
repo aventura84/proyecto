8cff4b907929bed011744f136df0c37509c0eff5
aventura84 <ventura_antonio@hotmail.com>
2022-06-24 12:56:11 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const pokemonContainer = document.ct(".pokemon-container")
+
+function traerPokemon(){
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=1126')
+    .then (res =>)
+}
+
+function (){
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=1126')
+
+}
+//
