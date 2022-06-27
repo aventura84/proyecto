311b4783f9aef4c34f965c6d9cd7d47101f1166b
aventura84 <ventura_antonio@hotmail.com>
2022-06-27 16:46:29 +0200

    nuevo

--- js/main.js
+++ js/main.js
@@ -5,22 +5,7 @@ let pokemons = [];
 const buscadorElement = document.forms.buscador;
 const nombreElement = buscador.elements.nombre;
 const listadoPokemonsUl = document.querySelector("#listadoPokemons");
-
-//documento.getElementById(id_name);
-//let limit = 8;
-//let offset = 1;
-// previous.addEventListener("click", () => {
-//   if (offset != 1) {
-//     offset -= 9;
-//     removeChildNodes(pokemonContainer);
-//     fetchPokemons(offset, limit);
-//   }
-// });
-// next.addEventListener("click", () => {
-//   offset += 9;
-//   removeChildNodes(pokemonContainer);
-//   fetchPokemons(offset, limit);
-// });
+const pokemonContainer = document.querySelector(".pokemon-container");
 
 const funcSubmit = (event) => {
   event.preventDefault();
@@ -63,7 +48,10 @@ async function render(pokemonsToPrint) {
     const detallePokemon = await fetchPokemon(pokemon.url);
     const pokemonLi = document.createElement("li");
     const pokemonImg = document.createElement("img");
-    pokemonImg.setAttribute("src", "https://"); // detallePokemon
+    pokemonImg.setAttribute(
+      "src",
+      "https://pokeapi.co/api/v2/pokemon/${Name}/"
+    );
     const pokemonName = document.createElement("p");
     pokemonName.textContent = pokemon.name;
     pokemonLi.append(pokemonImg);
@@ -73,19 +61,11 @@ async function render(pokemonsToPrint) {
   listadoPokemonsUl.append(fragmentPokemon);
 }
 
-function createPokemon(pokemon) {
-  const flipCard = document.createElement("div");
-  flipCard.classList.add("flip-card");
-
-  const cardContainer = document.createElement("div");
-  //cardContainer.classList.add("card-container");
-
-  flipCard.appendChild(cardContainer);
-
+function create(pokemon) {
   const card = document.createElement("div");
   card.classList.add("pokemon-block");
 
-  const spriteContainer = document.createElement("div");
+  const spriteContainer = document.createElemenent("dic");
   spriteContainer.classList.add("img-container");
 
   const sprite = document.createElement("img");
@@ -94,7 +74,7 @@ function createPokemon(pokemon) {
   spriteContainer.appendChild(sprite);
 
   const number = document.createElement("p");
-  number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
+  number.textContent = "#${pokemon.id.toString().padStart(3,0)}";
 
   const name = document.createElement("p");
   name.classList.add("name");
@@ -104,52 +84,5 @@ function createPokemon(pokemon) {
   card.appendChild(number);
   card.appendChild(name);
 
-  const cardBack = document.createElement("div");
-  cardBack.classList.add("pokemon-block-back");
-
-  cardBack.appendChild(progressBars(pokemon.stats));
-
-  cardContainer.appendChild(card);
-  cardContainer.appendChild(cardBack);
-  pokemonContainer.appendChild(flipCard);
-}
-
-function progressBars(stats) {
-  const statsContainer = document.createElement("div");
-  statsContainer.classList.add("stats-container");
-
-  for (let i = 0; i < 3; i++) {
-    const stat = stats[i];
-
-    const statPercent = stat.base_stat / 2 + "%";
-    const statContainer = document.createElement("stat-container");
-    statContainer.classList.add("stat-container");
-
-    const statName = document.createElement("p");
-    statName.textContent = stat.stat.name;
-
-    const progress = document.createElement("div");
-    progress.classList.add("progress");
-
-    const progressBar = document.createElement("div");
-    progressBar.classList.add("progress-bar");
-    progressBar.setAttribute("aria-valuenow", stat.base_stat);
-    progressBar.setAttribute("aria-valuemin", 0);
-    progressBar.setAttribute("aria-valuemax", 200);
-    progressBar.style.width = statPercent;
-
-    progressBar.textContent = stat.base_stat;
-
-    progress.appendChild(progressBar);
-    statContainer.appendChild(statName);
-    statContainer.appendChild(progress);
-
-    statsContainer.appendChild(statContainer);
-  }
-  return statsContainer;
-}
-function removeChildNodes(parent) {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
-  }
+  pokemonContainer.appendChild(card);
 }
